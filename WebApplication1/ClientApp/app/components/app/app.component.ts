5b7ddd7a8a261637a59d2300ac3941076722d33b
SunKD <sundavey@hotmail.com>
2018-07-19 21:37:14 -0700

    deleted unnecessary files

--- WebApplication1/ClientApp/app/components/app/app.component.ts
+++ WebApplication1/ClientApp/app/components/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    msg = "Click the submit button to reveal the picture."
+    InitialClick = false;
+
+    beach: boolean = true;
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onSubmit() {
+        this.InitialClick = true;
+        this.beach = !this.beach;
+    }
+
 }
+
